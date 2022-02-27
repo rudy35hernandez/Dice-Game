469cf26e9c853446b3f04b8cd2c1e1e520384818
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-02-27 10:40:23 -0800

    scoreboard working

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ let scoreTwo = 0;
 let playerOneTurn = true;
 
 document.getElementById("rollBtn").addEventListener("click", rollBtn)
+let playerOneScore = document.getElementById("player1Scoreboard")
+let playerTwoScore = document.getElementById("player2Scoreboard")
+
+
 
 function rollBtn(){
     
     if(playerOneTurn){
         playerOneTurn = false
-        console.log(playerOneTurn)
+        scoreOne +=  Math.ceil(Math.random() * 6)
+        console.log(scoreOne)
+        playerOneScore.innerHTML = scoreOne
         document.getElementById("message").innerHTML = "Player 2 Turn"
     } else {
         playerOneTurn = true;
-        console.log(playerOneTurn)
+        scoreTwo +=  Math.ceil(Math.random() * 6)
+        playerTwoScore.innerHTML = scoreTwo
+        console.log(scoreTwo)
         document.getElementById("message").innerHTML = "Player 1 Turn"
     }
 }
