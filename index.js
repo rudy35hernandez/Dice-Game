63c622d520783f79a20b0aa4e0238e2a4d329c62
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-03-03 18:07:40 -0800

    added active class to players turn

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ let scoreTwo = 0;
 let playerOneTurn = true;
 
 document.getElementById("rollBtn").addEventListener("click", rollBtn)
+let player1Dice = document.getElementById("player1Dice")
+let player2Dice = document.getElementById("player2Dice")
 let playerOneScore = document.getElementById("player1Scoreboard")
 let playerTwoScore = document.getElementById("player2Scoreboard")
+const resetBtn = document.getElementById("resetBtn")
+
+const message = document.getElementById("message")
 
 
 
@@ -15,15 +20,23 @@ function rollBtn(){
     
     if(playerOneTurn){
         playerOneTurn = false
-        scoreOne +=  Math.ceil(Math.random() * 6)
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        let diceRoll1 = Math.ceil(Math.random() * 6)
+        player1Dice.innerHTML = diceRoll1
         console.log(scoreOne)
+        scoreOne +=  diceRoll1
         playerOneScore.innerHTML = scoreOne
-        document.getElementById("message").innerHTML = "Player 2 Turn"
+        message.innerHTML = "Player 2 Turn"
     } else {
         playerOneTurn = true;
-        scoreTwo +=  Math.ceil(Math.random() * 6)
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        let diceRoll2 = Math.ceil(Math.random() * 6)
+        player2Dice.innerHTML = diceRoll2
+        scoreTwo +=  diceRoll2
         playerTwoScore.innerHTML = scoreTwo
         console.log(scoreTwo)
-        document.getElementById("message").innerHTML = "Player 1 Turn"
+        message.innerHTML = "Player 1 Turn"
     }
 }
